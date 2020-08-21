aca138f778de43d832ae625866c290a04d9229c4
kksj216 <21900044@handong.edu>
2020-08-21 14:15:34 +0900

    로그인기능 구현

--- src/main/java/net/project/web/UserController.java
+++ src/main/java/net/project/web/UserController.java
@@ -1,5 +1,7 @@
 package net.project.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,31 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userID, String password, HttpSession session) {
+		User user = userRepository.findByUserID(userID);
+		
+		if (user == null) {
+			System.out.println("Login fail! at user == null");
+			return "redirect:/users/loginForm";
+		}
+
+		if (!password.equals(user.getPassword())) {
+			System.out.println("Login fail! at password..");
+			return "redirect:/users/loginForm"; 
+		}
+		
+		System.out.println("Login Success!");
+		session.setAttribute("user", user); 
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("/form")
 	public String form() {
