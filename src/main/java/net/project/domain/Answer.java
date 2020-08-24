1e7217126033aa155b69685ca6df7ba7ab2e02c7
kksj216 <21900044@handong.edu>
2020-08-24 20:44:14 +0900

    답변삭제 기능

--- src/main/java/net/project/domain/Answer.java
+++ src/main/java/net/project/domain/Answer.java
@@ -52,6 +52,10 @@ public class Answer {
 		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 	}
 	
+	public boolean isSameWriter(User loginUser) {
+		return loginUser.equals(this.writer);
+	}	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/net/project/web/ApiAnswerController.java
+++ src/main/java/net/project/web/ApiAnswerController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import net.project.domain.Answer;
 import net.project.domain.AnswerRepository;
 import net.project.domain.Question;
 import net.project.domain.QuestionRepository;
+import net.project.domain.Result;
 import net.project.domain.User;
 
 @RestController
@@ -36,4 +38,20 @@ public class ApiAnswerController {
 		return answerRepository.save(answer);
 		//return String.format("redirect:/questions/%d", questionId); 
 	}
+	
+	@DeleteMapping("/{id}")
+	public Result delete(@PathVariable Long questionId, @PathVariable Long id, HttpSession session) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return Result.fail("로그인해야 합니다.");
+		}
+		
+		Answer answer = answerRepository.findById(id).get(); // findOne(id)
+		User loginUser = HttpSessionUtils.getUserFromSession(session); 
+		if (!answer.isSameWriter(loginUser)) {
+			return Result.fail("자신의 글만 삭제할 수 있습니다.");
+		}
+		answerRepository.deleteById(id);
+		return Result.ok();
+ 	}
+	
 }
